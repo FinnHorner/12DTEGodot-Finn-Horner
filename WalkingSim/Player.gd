acd46f97945ad9801e7ef3e2cefb4cd1d05b2cd4
FinnHorner <finhor35@kingshigh.school.nz>
2023-06-28 13:26:35 +1200

    28/06

--- WalkingSim/Player.gd
+++ WalkingSim/Player.gd
@@ -1,7 +1,10 @@
 extends CharacterBody3D
 
-@export var mouse_sensitvity = 0.001
-const SPEED = 5.0
+@export var mouse_sensitivity = 0.001
+@export var run_speed = 40
+@export var walk_speed = 30
+@export var FRICTION = 1
+var SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -9,16 +12,20 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	
+
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
-		rotate_y(-event.relative.x * mouse_sensitvity)
-		$Camera3D.rotate_x(-event.relative.y * mouse_sensitvity)
-		$Camera3D.rotation.x = clamp($Camera3D.rotation.x,  -1.2,1.2)
-
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, -1.2, 1.2)
 
 func _physics_process(delta):
 	# Add the gravity.
+	if Input.is_action_pressed("Run"):
+		SPEED = run_speed
+	else:
+		SPEED = walk_speed
+
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
@@ -28,13 +35,13 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("Left", "Right", "Up", "Down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = move_toward(velocity.x, direction.x * SPEED, FRICTION)
+		velocity.z = move_toward(velocity.z, direction.z * SPEED, FRICTION)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, FRICTION)
+		velocity.z = move_toward(velocity.z, 0, FRICTION)
 
 	move_and_slide()
